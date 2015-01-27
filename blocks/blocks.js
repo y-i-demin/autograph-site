0df945c8b74de15c066ec0a2eed7f32ac43d7883
y-i-demin <y.i.demin@yandex.ru>
2015-01-28 02:41:51 +0300

    portfolio script draft

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -5,3 +5,33 @@
     n = "createElementNS";
     e[c] = e[c][r]("is-js_no", "is-js_yes");
 })(document);
+
+if (!console) {
+    window.console = {
+        log: function() {}
+    };
+}
+
+window.app = {
+    blocks: {},
+    init: function() {
+        this.paranja = $('<div/>', {
+            'class': 'js-paranja paranja g-hidden'
+        });
+
+        this.paranja.appendTo('body');
+
+        for (var block in this.blocks) {
+            if (this.blocks[block].init && typeof this.blocks[block].init == "function") {
+                this.blocks[block].init();
+            }
+
+            console.log(block + ' init');
+        }
+    }
+};
+
+$(function(){
+    window.app.init();
+});
+
---
+++ blocks/portfolio/portfolio.js
@@ -0,0 +1,55 @@
+(function(){
+    app.blocks.portfolio = {
+        current: undefined,
+        bindEvents: function() {
+            $(document).on('click', '.js-portfolio-item', this.showItem.bind(this));
+        },
+
+        showItem: function(event) {
+            event.preventDefault();
+
+            app.paranja.removeClass('g-hidden');
+            this.box.removeClass('g-hidden');
+
+            var self = this;
+            var maxWidth = this.box.width();
+            var maxHeight = this.box.height();
+            var elem = $(event.target);
+            var img = new Image();
+
+            img.src = elem.attr('href');
+            img.onload = function() {
+                img.style.maxHeight = maxHeight + 'px';
+                img.style.maxWidth = maxWidth + 'px';
+                img.className = 'portfolio-box-image';
+                self.box.html(img);
+            };
+
+            this.current = $('.js-portfolio-item').index(elem);
+        },
+
+        init: function() {
+            var portfolio = $('.js-portfolio');
+
+            if (!portfolio.get(0)) {
+                return;
+            }
+            
+            $('.js-portfolio-item', portfolio).each(function() {
+                var preload = new Image();
+                preload.src = this.href;
+                preload.onload = function() {
+                    console.log(preload.src + ' loaded');
+                }
+            });
+
+            this.box = $('<div/>', {
+                'class': 'js-portfolio-box portfolio-box g-hidden'
+            });
+
+
+            this.box.appendTo('body');
+            this.bindEvents();
+        }
+    }
+})();
